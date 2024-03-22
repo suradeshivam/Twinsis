282eef9797e015af73173a255f8297548e0ad198
shivam surade <suradeshivamshyam@gmail.com>
2024-03-22 22:35:43 +0530

    added path of netlify to cors

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,21 @@ const connectDB = require("./config/db");
 const user = require("./routes/userRoutes");
 const appointment = require("./routes/appointementRoutes");
 const doctor = require("./routes/doctorRoutes");
+const cors = require("cors");
 
 const app = express();
 dotenv.config();
 connectDB();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://65fdb9a0071f062da561aa6a--beautiful-horse-b76c05.netlify.app",
+    ],
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("<h1> Working <h1>");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar.jsx";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage.jsx";
-import Profile from "./Components/Profile.jsx";
+import Profile1 from "./Components/Profile1.jsx";
 import Appointments from "./Components/Appointments.jsx";
 import Appointment from "./Components/Appointment.jsx";
 import Book from "./Components/Book.jsx";
@@ -23,7 +23,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="book" element={<Book />} />
         <Route path="doctor" element={<DoctorDashboarad />}>
-          <Route path="" element={<Profile />} />
+          <Route path="" element={<Profile1 />} />
           <Route path="appointments" element={<Appointments />} />
           <Route path="appointment" element={<Appointment />} />
           <Route path="history" element={<DocHistory />} />
